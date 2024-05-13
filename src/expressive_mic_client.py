aa09f1915c302311d2808a2b38c02ee2688d392e
zach <zachx121@outlook.com>
2024-05-13 21:26:32 +0800

    目前v2停顿检测做的还行

--- src/expressive_mic_client.py
+++ src/expressive_mic_client.py
@@ -29,7 +29,7 @@ def send_audio(buffer):
 # Callback function for audio input
 TIME_TAG = -1
 BUFFER_CACHE = b""
-RMS_HOLD = 700
+SAMPLE_SEC, PAUSE_SEC, RMS_HOLD = 0.05, 0.5, 500  # 停顿检测的时长、音量; 控制在0.05s执行一次call_back 注意采样频率快了rms_hold得降低
 IS_APPENDING = False
 def callback(in_data, frame_count, time_info, status):
     global TIME_TAG, BUFFER_CACHE, RMS_HOLD, IS_APPENDING
@@ -57,6 +57,40 @@ def callback(in_data, frame_count, time_info, status):
         BUFFER_CACHE = in_data
     return None, pyaudio.paContinue
 
+# 检查buffer末尾，倒查是否有持续达0.2s的停顿，音频输入采样是0.05s一个segment所以应该不会出现AB两句话中间检测不到
+def check_pause(audio_buffer, pause_sec, rms_hold, bytes_sec):
+    if len(audio_buffer) > int(pause_sec*bytes_sec):
+        # todo 这里是直接按采样buffer的末尾往回查0.2s停顿，是不是该从采样buffer最后一个小于rms_hold的0.05s片段开始往回查0.2s停顿？
+        rms = audioop.rms(audio_buffer[-int(pause_sec*bytes_sec):], 2)
+        if rms <= rms_hold:
+            return True
+    # 例如检测0.1s停顿，但音频长度不够0.1s，默认返回False表示无停顿需要继续追加buffer
+    # 长度够了但是最后0.1s
+    return False
+
+START_APPEND = False
+def callback_v2(in_data, frame_count, time_info, status):
+    global TIME_TAG, START_APPEND, BUFFER_CACHE, RMS_HOLD
+    rms = audioop.rms(in_data, 2)
+    if rms >= RMS_HOLD and not START_APPEND:
+        TIME_TAG = int(time.time())
+        print(f"Start Append (rms: {rms}>={RMS_HOLD} time: {TIME_TAG}).")
+        START_APPEND = True
+
+    if START_APPEND:
+        BUFFER_CACHE += in_data
+        # 检查buffer末尾，倒查是否有持续达0.2s的停顿，音频输入采样是0.05s一个segment所以应该不会出现AB两句话中间检测不到
+        cond_pause = check_pause(BUFFER_CACHE, pause_sec=PAUSE_SEC, rms_hold=RMS_HOLD, bytes_sec=BYTES_PER_SEC)
+        if cond_pause:
+            print(f"Save And Send... TIME_TAG:{TIME_TAG} buffer_len({len(BUFFER_CACHE)}):{len(BUFFER_CACHE)/BYTES_PER_SEC:.2f}s")
+            scipy.io.wavfile.write(f"req_audio_{TIME_TAG}.wav", SAMPLE_RATE, np.frombuffer(BUFFER_CACHE, dtype=np.int16))
+            # send_audio(BUFFER_CACHE)
+            # 重置
+            BUFFER_CACHE = b""
+            START_APPEND = False
+    else:
+        BUFFER_CACHE = in_data
+    return None, pyaudio.paContinue
 
 # Initialize PyAudio
 audio = pyaudio.PyAudio()
@@ -66,8 +100,8 @@ stream = audio.open(format=pyaudio.paInt16,
                     channels=CHANNEL,
                     rate=SAMPLE_RATE,
                     input=True,
-                    frames_per_buffer=int(BYTES_PER_SEC*0.25),  # 控制在0.25s执行一次call_back
-                    stream_callback=callback)
+                    frames_per_buffer=int(BYTES_PER_SEC*SAMPLE_SEC),  # 控制在0.05s执行一次call_back 注意采样频率快了rms_hold得降低
+                    stream_callback=callback_v2)
 
 print("Recording...")
 
