4f1266e54817976c226cbffd148526eec0564cec
zach <zachx121@outlook.com>
2024-05-13 22:01:34 +0800

    ExpressiveModel修复完成

---
+++ src/expressive_model.py
@@ -0,0 +1,145 @@
+import torch
+from pathlib import Path
+from fairseq2.data import SequenceData
+from fairseq2.data.audio import WaveformToFbankConverter
+import logging
+from seamless_communication.cli.expressivity.predict.pretssel_generator import (
+    PretsselGenerator,
+)
+from seamless_communication.cli.m4t.predict import (
+    add_inference_arguments,
+    set_generation_opts,
+)
+from seamless_communication.inference import Translator
+from seamless_communication.models.unity import (
+    load_gcmvn_stats,
+    load_unity_unit_tokenizer,
+)
+from seamless_communication.store import add_gated_assets
+
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+
+class args:
+    tgt_lang = 'eng'
+    duration_factor=0.8
+    gated_model_dir=Path('/root/autodl-fs/SeamlessExpressive')
+    model_name='seamless_expressivity'
+    no_repeat_ngram_size=4
+    src_lang=None
+    task=None
+    text_generation_beam_size=5
+    text_generation_max_len_a=1
+    text_generation_max_len_b=200
+    text_generation_ngram_blocking=False
+    text_unk_blocking=False
+    unit_generation_beam_size=5
+    unit_generation_max_len_a=25
+    unit_generation_max_len_b=50
+    unit_generation_ngram_blocking=False
+    unit_generation_ngram_filtering=False
+    vocoder_name='vocoder_pretssel'
+
+
+class ExpressiveModel:
+    def __init__(self, sample_rate=16000):
+        self.sample_rate = sample_rate
+        add_gated_assets(args.gated_model_dir)
+        unit_tokenizer = load_unity_unit_tokenizer(args.model_name)
+        # 通用Translator框架 (文本->文本、音频->文本、文本->音频 etc.)
+        self.translator = Translator(
+                args.model_name,
+                vocoder_name_or_card=None,
+                device=device,
+                dtype=dtype,
+            )
+
+        self.pretssel_generator = PretsselGenerator(
+                args.vocoder_name,
+                vocab_info=unit_tokenizer.vocab_info,
+                device=device,
+                dtype=dtype,
+            )
+
+
+        self.fbank_extractor = WaveformToFbankConverter(
+                num_mel_bins=80,
+                waveform_scale=2**15,
+                channel_last=True,
+                standardize=False,
+                device=device,
+                dtype=dtype,
+            )
+        _gcmvn_mean, _gcmvn_std = load_gcmvn_stats(args.vocoder_name)
+        self.gcmvn_mean = torch.tensor(_gcmvn_mean, device=device, dtype=dtype)
+        self.gcmvn_std = torch.tensor(_gcmvn_std, device=device, dtype=dtype)
+
+        self.text_generation_opts, self.unit_generation_opts = set_generation_opts(args)
+
+    @staticmethod
+    def remove_prosody_tokens_from_text(text: str) -> str:
+        # filter out prosody tokens, there is only emphasis '*', and pause '='
+        text = text.replace("*", "").replace("=", "")
+        text = " ".join(text.split())
+        return text
+
+    def predict(self, wav, duration_factor=None, tgt_lang=None):
+        duration_factor = args.duration_factor if duration_factor is None else duration_factor
+        tgt_lang = args.tgt_lang if tgt_lang is None else tgt_lang
+        data = self.fbank_extractor(
+            {
+                "waveform": wav,
+                "sample_rate": self.sample_rate,
+            }
+        )
+        fbank = data["fbank"]
+        gcmvn_fbank = fbank.subtract(self.gcmvn_mean).divide(self.gcmvn_std)
+        std, mean = torch.std_mean(fbank, dim=0)
+        fbank = fbank.subtract(mean).divide(std)
+
+        src = SequenceData(
+            seqs=fbank.unsqueeze(0),
+            seq_lens=torch.LongTensor([fbank.shape[0]]),
+            is_ragged=False,
+        )
+        src_gcmvn = SequenceData(
+            seqs=gcmvn_fbank.unsqueeze(0),
+            seq_lens=torch.LongTensor([gcmvn_fbank.shape[0]]),
+            is_ragged=False,
+        )
+
+        text_output, unit_output = self.translator.predict(
+            input=src,
+            task_str="s2st",
+            tgt_lang=tgt_lang,
+            text_generation_opts=self.text_generation_opts,
+            unit_generation_opts=self.unit_generation_opts,
+            unit_generation_ngram_filtering=args.unit_generation_ngram_filtering,
+            duration_factor=duration_factor,
+            prosody_encoder_input=src_gcmvn,  # 通过配置src_gcmvn应该可以实现指定音色，这是个字典，有一个key存到tensor
+        )
+        speech_output = self.pretssel_generator.predict(
+            units=unit_output.units,
+            tgt_lang=tgt_lang,
+            prosody_encoder_input=src_gcmvn,
+        )
+
+        wav_arr = speech_output.audio_wavs[0][0].to(torch.float32).cpu().numpy()
+        wav_sr = speech_output.sample_rate
+        text = self.remove_prosody_tokens_from_text(str(text_output[0]))
+        return wav_arr, wav_sr, text
+
+    def predict_file(self, in_file, duration_factor=None, tgt_lang=None):
+        import torchaudio
+        wav, sample_rate = torchaudio.load(in_file)
+        logging.debug("原音频信息", wav.shape, wav.dtype, sample_rate)
+        wav = torchaudio.functional.resample(wav, orig_freq=sample_rate, new_freq=16_000)
+        # wav[1] = wav[0]  # Debug 尝试做成单通道模拟双通道的模式？
+        logging.debug("重采样音频信息", wav.shape, wav.dtype)
+        wav = wav.transpose(0, 1)
+        logging.debug("最终输入音频信息", wav.shape, wav.dtype)
+        return self.predict(wav, duration_factor=duration_factor, tgt_lang=tgt_lang)
+
+
